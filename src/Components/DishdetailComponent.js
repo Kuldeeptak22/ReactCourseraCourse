bbb1dcffa853279cfe49c02e75e03a2e4b2ac85e
Kuldeep tak <Kuldeeptak2211@gmail.com>
2023-04-16 16:06:50 +0530

    feat: React Forms and Redux

--- src/Components/DishdetailComponent.js
+++ src/Components/DishdetailComponent.js
@@ -7,8 +7,20 @@ import {
   CardTitle,
   Breadcrumb,
   BreadcrumbItem,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Label,
+  Row,
+  Col,
 } from "reactstrap";
+import { Control, LocalForm, Errors, Field } from "react-redux-form";
 import { Link } from "react-router-dom";
+
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !val || val.length <= len;
+const minLength = (len) => (val) => val && val.length >= len;
 
 function RenderDish({ dish }) {
   return (
@@ -47,10 +59,121 @@ function RenderComments({ comments }) {
       <div className=" col-12 col-md-5 m-1">
         <h4>Comments</h4>
         <ul className="list-unstyled">{commentListItems}</ul>
+        <CommentForm />
       </div>
     );
   } else {
     return <div></div>;
+  }
+}
+
+export class CommentForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleComment = this.handleComment.bind(this);
+
+    this.state = {
+      isModalOpen: false,
+    };
+
+    this.toggleModal = this.toggleModal.bind(this);
+  }
+
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen,
+    });
+  }
+  handleComment(values) {
+    console.log("Current State is: " + JSON.stringify(values));
+    alert("Current State is: " + JSON.stringify(values));
+  }
+
+  render() {
+    return (
+      <>
+        <Button className="btn btn-primary" outline onClick={this.toggleModal}>
+          <i className="fa fa-solid fa-pencil"></i> Submit Comment
+        </Button>
+
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+          <ModalBody>
+            <LocalForm onSubmit={(values) => this.handleComment(values)}>
+              <Row className="form-group">
+                <Label htmlFor="rating" md={12}>
+                  Rating
+                </Label>
+                <Col md={12}>
+                  <Control.select
+                    defaultValue="1"
+                    model=".rating"
+                    className="rating"
+                  >
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                  </Control.select>
+                </Col>
+              </Row>
+              <Row className="form-group">
+                <Label htmlFor="yourname" md={12}>
+                  Your Name
+                </Label>
+                <Col md={12}>
+                  <Control.text
+                    model=".yourname"
+                    id="yourname"
+                    name="yourname"
+                    placeholder="Your Name"
+                    className="form-control"
+                    validators={{
+                      required,
+                      minLength: minLength(3),
+                      maxLength: maxLength(15),
+                    }}
+                  />
+                  <Errors
+                    className="text-danger"
+                    model=".yourname"
+                    show="touched"
+                    messages={{
+                      required: "*Required:",
+                      minLength: " Must be greater than 2 characters",
+                      maxLength: " Must be 15 characters or less",
+                    }}
+                  />
+                </Col>
+              </Row>
+              <Row className="form-group">
+                <Label htmlFor="message" md={12}>
+                  Comment
+                </Label>
+                <Col md={12}>
+                  <Control.textarea
+                    model=".message"
+                    id="message"
+                    name="message"
+                    rows="6"
+                    className="form-control"
+                  />
+                </Col>
+              </Row>
+              <Row className="form-group">
+                <Col md={{ size: 10 }}>
+                  <Button type="submit" color="primary">
+                    Submit
+                  </Button>
+                </Col>
+              </Row>
+            </LocalForm>
+          </ModalBody>
+        </Modal>
+      </>
+    );
   }
 }
 
