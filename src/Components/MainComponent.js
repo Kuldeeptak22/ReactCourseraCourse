f6d92e8e4de3d043869fa85765918dd085d58d7f
Kuldeep tak <Kuldeeptak2211@gmail.com>
2023-04-24 19:50:03 +0530

    feat: React-Redux-Form revisited

--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -9,6 +9,7 @@ import Footer from "./FooterComponent";
 import { Switch, Route, Redirect, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import { addComment, fetchDishes } from "../redux/actions/ActionCreators";
+import { actions } from "react-redux-form";
 
 class Main extends Component {
   constructor(props) {
@@ -66,7 +67,13 @@ class Main extends Component {
             component={() => <Menu dishes={this.props.dishes} />}
           />
           <Route path="/menu/:dishId" component={DishWithId} />
-          <Route exact path="/contactus" component={Contact} />
+          <Route
+            exact
+            path="/contactus"
+            component={() => (
+              <Contact resetFeedbackForm={this.props.resetFeedbackForm} />
+            )}
+          />
           <Redirect to="/home" />
         </Switch>
         <Footer />
@@ -90,5 +97,9 @@ const mapDispatchToProps = (dispatch) => ({
   fetchDishes: () => {
     dispatch(fetchDishes());
   },
+  resetFeedbackForm: () => {
+    dispatch(actions.reset("feedback"));
+  },
 });
+
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createForms } from "react-redux-form";
 import { Dishes } from "./reducers/dishes";
 import { Comments } from "./reducers/comments";
 import { Promotions } from "./reducers/promotions";
 import { Leaders } from "./reducers/leaders";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
+import { InitialFeedback } from "./reducers/forms";
 
 export const configureStore = () => {
   const store = createStore(
@@ -13,6 +15,9 @@ export const configureStore = () => {
       comments: Comments,
       promotions: Promotions,
       leaders: Leaders,
+      ...createForms({
+        feedback: InitialFeedback,
+      }),
     }),
     applyMiddleware(thunk, logger)
   );
