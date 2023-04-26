2a77f1234e86029b14c2aca365179e8c14bae541
Kuldeep tak <Kuldeeptak2211@gmail.com>
2023-04-26 19:50:33 +0530

    feat : Fetch from Server

--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -8,7 +8,12 @@ import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import { Switch, Route, Redirect, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
-import { addComment, fetchDishes } from "../redux/actions/ActionCreators";
+import {
+  addComment,
+  fetchDishes,
+  fetchComments,
+  fetchPromos,
+} from "../redux/actions/ActionCreators";
 import { actions } from "react-redux-form";
 
 class Main extends Component {
@@ -18,6 +23,8 @@ class Main extends Component {
 
   componentDidMount() {
     this.props.fetchDishes();
+    this.props.fetchComments();
+    this.props.fetchPromos();
   }
 
   render() {
@@ -26,8 +33,14 @@ class Main extends Component {
         <Home
           dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
           dishesLoading={this.props.dishes.isLoading}
-          dishErrMessage={this.props.dishes.errMessage}
-          promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+          dishErrMess={this.props.dishes.errMess}
+          promotion={
+            this.props.promotions.promotions.filter(
+              (promo) => promo.featured
+            )[0]
+          }
+          promoLoading={this.props.promotions.isLoading}
+          promoErrMess={this.props.promotions.errMess}
           leader={this.props.leaders.filter((leader) => leader.featured)[0]}
         />
       );
@@ -46,10 +59,11 @@ class Main extends Component {
             )[0]
           }
           isLoading={this.props.dishes.isLoading}
-          errMessage={this.props.dishes.errMessage}
-          comments={this.props.comments.filter(
+          errMess={this.props.dishes.errMess}
+          comments={this.props.comments.comments.filter(
             (comment) => comment.dishId === parseInt(match.params.dishId, 10)
           )}
+          commentsErrMess={this.props.comments.errMess}
           addComment={this.props.addComment}
         />
       );
@@ -100,6 +114,8 @@ const mapDispatchToProps = (dispatch) => ({
   resetFeedbackForm: () => {
     dispatch(actions.reset("feedback"));
   },
+  fetchComments: () => dispatch(fetchComments()),
+  fetchPromos: () => dispatch(fetchPromos()),
 });
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
